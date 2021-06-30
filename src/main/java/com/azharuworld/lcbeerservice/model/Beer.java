1801d2b073011ca41ceae709f54c980ca95a5338
AzharSy3d <azharuworld@gmail.com>
2021-07-01 00:18:27 +0530

    LC-0005 : Create JPA entity for Beer, closes #5

---
+++ src/main/java/com/azharuworld/lcbeerservice/model/Beer.java
@@ -0,0 +1,50 @@
+package com.azharuworld.lcbeerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, nullable = false,updatable = false,columnDefinition = "varchar")
+    private UUID id;
+    @Version
+    private Integer version;
+
+
+    private String beerName;
+    private String beerStyle;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    @Column(updatable = false)
+    private Timestamp updatedDate;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
