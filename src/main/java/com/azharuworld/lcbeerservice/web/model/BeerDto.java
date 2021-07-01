8be25cd6dd59bbc6d17f098fad691de77b1306fc
AzharSy3d <azharuworld@gmail.com>
2021-07-02 00:45:35 +0530

    LC-0012 : Add Jackson config to BeerDto, closes#12

--- src/main/java/com/azharuworld/lcbeerservice/web/model/BeerDto.java
+++ src/main/java/com/azharuworld/lcbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.azharuworld.lcbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,8 +25,10 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotNull
@@ -33,6 +36,7 @@ public class BeerDto {
     @NotNull
     private BeerStyleEnum beerStyle;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @NotNull
     @Positive
     private BigDecimal price;
